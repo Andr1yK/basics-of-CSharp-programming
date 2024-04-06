e286b922c7722430502ea23445563e1ffc6237d5
Andriy Kobetiak <andriy.k@mate.academy>
2024-04-06 19:11:03 +0300

    implement-lab-6

---
+++ Lab_6/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Lab_6
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.Write("Input string: ");
+      String input = Console.ReadLine() ?? throw new InvalidOperationException();
+      
+      Console.WriteLine($"Count of commas: {CountAllCommas(input)}");
+
+      Console.WriteLine($"Replaced commas: {ReplaceAllCommas(input)}");
+    }
+    
+    static int CountAllCommas(String input)
+    {
+      int count = 0;
+      
+      foreach (char c in input)
+      {
+        if (c == ',')
+        {
+          count++;
+        }
+      }
+      
+      return count;
+    }
+    
+    static string ReplaceAllCommas(String input)
+    {
+      return input.Replace(",", "**");
+    }
+  }
+}
